864817bdd4c19f9e8b385fc50d56b2f698773e1e
Ibndaud <107104641+Ibndaud@users.noreply.github.com>
2025-08-16 15:51:07 +0300

    Add files via upload

--- analyze_logs.sh
+++ analyze_logs.sh
@@ -3,13 +3,13 @@
 {
  echo "Отчет о логе веб-сервера"
  echo "========================"
- echo -e "Общее количество запросов:\t  $(awk 'END{print NR}' "access.log")"
- echo -e "Количество уникальных IP-адресов:\t  $(awk 'NF { ips[$1]=1} END { for (ip in ips) count++; print count }' "access.log")"
+ echo -e "Общее количество запросов:\t  $(awk 'END{print NR}' access.log)"
+ echo -e "Количество уникальных IP-адресов:\t $(awk 'NF { ips[$1] } END { print length(ips) }' access.log)"
  echo
  echo
  echo "Количество запросов по методам:"
- echo "   $(awk '/GET/ { count++ } END { print count }' "access.log") GET"
- echo "   $(awk '/POST/ { count++ } END { print count }' "access.log") POST"
+ echo "   $(awk '/GET/ { count++ } END { print count }' access.log) GET"
+ echo "   $(awk '/POST/ { count++ } END { print count }' access.log) POST"
  echo
  echo
  echo -e "Самый популярный URL:\t $(awk 'match($0, /"[^"]+"/) {
@@ -21,7 +21,7 @@
        if (counts[url] > max) { max = counts[url]; top = url }
      }
    }
-   END { print max, top }' "access.log")"
+   END { print max, top }' access.log)"
 } > report.txt
 
 echo "Отчет сохранен в файл report.txt"
